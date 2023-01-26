fe75c6ad8740b93ede7f40e37e59a944e3867f2c
=Paras <=>
2023-01-26 21:38:02 +0530

    Weather API using

---
+++ location.js
@@ -0,0 +1,78 @@
+let loadCountries = function(){
+    fetch('https://countriesnow.space/api/v0.1/countries/positions').
+    then((response) => {
+        return response.json();
+    })
+    .then((text) => {
+        console.log(text['msg']);
+        let countryArr = text['data'];
+        countryArr.forEach(element => {
+            let select = document.getElementById('country');
+            let opt = document.createElement('option');
+            opt.innerText = element['name'];
+            opt.value = element['name'];
+            select.appendChild(opt);
+        });       
+    })
+};
+
+loadCountries();
+
+function loadStates(){
+   document.getElementById("state").options.length = 0;
+   document.getElementById("city").options.length = 0;
+   let selectedCountry = document.getElementById('country').value;
+   console.log(document.getElementById('country').value);
+    fetch('https://countriesnow.space/api/v0.1/countries/states', {
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },    
+          body: new URLSearchParams({
+            "country" : selectedCountry
+          }),
+    }).then((response) => {
+        return response.json();
+    }).then((text)=> {
+        console.log(text);
+        let stateArr = text['data'].states;
+        stateArr.forEach((item) => {
+            let selectState = document.getElementById('state');
+            let stateOpt = document.createElement('option');
+            stateOpt.innerText = item['name'];
+            stateOpt.value = item['name'];
+            selectState.appendChild(stateOpt);
+        })
+    });
+}
+
+
+function loadCities(){
+    let selectedState = document.getElementById('state').value;
+    let selectedCountry = document.getElementById('country').value;
+    console.log(selectedCountry);
+    console.log(selectedState);
+    fetch('https://countriesnow.space/api/v0.1/countries/state/cities',{
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },    
+        body: new URLSearchParams({
+            "state" : selectedState,
+            "country": selectedCountry
+          }),
+    }).then((response)=> {
+        return response.json();
+    }).then((text) => {
+        console.log(text);
+        let cityArr = text['data'];
+        console.log(cityArr[0]);
+        cityArr.forEach((item) => {
+            let selectCity = document.getElementById('city');
+            let cityOpt = document.createElement('option');
+            cityOpt.innerText = item;
+            cityOpt.value = item;
+            selectCity.appendChild(cityOpt);
+        })
+    });
+}
